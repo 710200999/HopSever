41f7edad33fe892394dea751afa3447c10f97817
710200999 <198722707+710200999@users.noreply.github.com>
2026-02-23 19:14:32 +0700

    Add files via upload

---
+++ HopSeverLowPlayer.lua
@@ -0,0 +1,31 @@
+-- Server Hop Roblox (ít người nhất)
+
+local HttpService = game:GetService("HttpService")
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+
+local PlaceID = game.PlaceId
+local JobID = game.JobId
+
+local function ServerHop()
+    local servers = {}
+    local req = game:HttpGet(
+        "https://games.roblox.com/v1/games/"..PlaceID.."/servers/Public?sortOrder=Asc&limit=100"
+    )
+    
+    local data = HttpService:JSONDecode(req)
+
+    for i, v in pairs(data.data) do
+        if v.playing < v.maxPlayers and v.id ~= JobID then
+            table.insert(servers, v.id)
+        end
+    end
+
+    if #servers > 0 then
+        TeleportService:TeleportToPlaceInstance(PlaceID, servers[math.random(1, #servers)], Players.LocalPlayer)
+    else
+        print("No other servers found!")
+    end
+end
+
+ServerHop()
